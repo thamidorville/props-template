9f7c1c3c78fb1a332e65c57560e734e921bec1e2
thamidorville <thamisdorville@gmail.com>
2023-01-18 17:33:35 -0300

    props-template

--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,56 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
   return (
     <div>
-      <h1>Garagem da Labenu</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem da {props.nome}</h1>
+      <button onClick={props.mensagemApresentacao} >Clique em mim</button>
+    
+      <Carro 
+      adicionadoPor = {props.nome}
+      cor ={"Preto"}
+      ano={"2022"}
+      flex={"True"}
+      />
+      <Carro adicionadoPor = {props.nome}
+      cor ={"Branco"}
+      ano={"2001"}
+      flex={"True"}
+      />
+      <Carro 
+      adicionadoPor = {props.nome}
+      cor={"Vermelho"}
+      ano={"1998"}
+      flex={"True"}
+      />
+      <Carro 
+      adicionadoPor = {props.nome}
+      cor={"Roxo"}
+      ano={"1990"}
+      flex={"False"}
+      />
+
+      <Carro adicionadoPor = {props.nome}
+      cor={"vinho"}
+      ano={1995}
+      flex={"True"}
+      />
+
+      <Carro 
+      adicionadoPor = {props.nome}
+      cor = {"laranja"}
+      ano = {2011}
+      flex = {"False"}
+      />
+
+      <Carro 
+      adicionadoPor = {props.nome}
+      cor ={"rosa"}
+      ano ={1996}
+      flex={"False"}
+      />
+
+
     </div>
   );
 }
